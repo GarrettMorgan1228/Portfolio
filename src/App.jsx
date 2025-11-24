e9e4dc796a12545f10400c4dbbfa30269591212a
GarrettMorgan1228 <garrettstevenmorgan@gmail.com>
2025-11-24 05:44:14 -0800

    changed landing page section paragraphs

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,15 @@ function App() {
         <div className='page-layout'>
           <main className='main'>
             <Section id='home' title='Home'>
-              <p>Short intro goes here. Replace with more JSX later.</p>
+              <p>Welcome to the landing page for Garrett's Computer Science project porfolio.</p>
             </Section>
 
             <Section id='about' title='About'>
-              <p>My name is Garrett Morgan and I like computers and science.</p>
+              <p>My name is Garrett Morgan and I am a Computer Science student finishing my AS
+                 at Clovis Community College. This page is meant to serve as not only a way for
+                 me to practice front-end programming, but also as a hub for me to host other
+                 projects I work on over time, enabling others to view my work in one
+                 centralized location. Please explore my projects I have listed below.</p>
             </Section>
 
             <ProjectsSection/>
